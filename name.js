7f5c6156154554b1b9e668926cc3cee49e64b59e
Bizet Rodriguez <brodriguez-velez@ucmerced.edu>
2017-11-19 00:02:12 -0800

    Removed name files

--- name.js
+++
@@ -1,5 +0,0 @@
-document.getElementsByTagName('button')[0].addEventListener('click', function(r) {
-	var yourName = document.getElementsByTagName('input')[0].value;
-
-	window.location.href = "index.html";
-});
